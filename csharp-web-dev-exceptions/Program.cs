13e11210e3c29c0bab81152a0dd0a9619ea96609
Tanya Buechel <tanyabuechel@gmail.com>
2022-09-28 16:43:36 -0500

    Finished exercise

--- csharp-web-dev-exceptions/Program.cs
+++ csharp-web-dev-exceptions/Program.cs
@@ -7,30 +7,63 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (y == 0.0)
+            {
+                throw new ArgumentOutOfRangeException("y", "You cannot divide by zero!");
+            }
+            else
+            {
+                return x / y;
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
-        }
-        */
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentNullException("fileName", "Must contain file name!");
+            }
+            else
+            {
+                if (fileName.EndsWith(".cs"))
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
 
-        static void Main(string[] args)
-        {
-            // Test out your Divide() function here!
 
-            // Test out your CheckFileExtension() function here!
-            Dictionary<string, string> students = new Dictionary<string, string>();
-            students.Add("Carl", "Program.cs");
-            students.Add("Brad", "");
-            students.Add("Elizabeth", "MyCode.cs");
-            students.Add("Stefanie", "CoolProgram.cs");
+            static void Main(string[] args)
+            {
+                 Test out your Divide() function here!
+                try
+                {
+                    Divide(6, 0);
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
 
+                // Test out your CheckFileExtension() function here!
+                Dictionary<string, string> students = new Dictionary<string, string>();
+                students.Add("Carl", "Program.cs");
+                students.Add("Brad", "");
+                students.Add("Elizabeth", "MyCode.cs");
+                students.Add("Stefanie", "CoolProgram.cs");
 
+                foreach(string fileName in students.Values)
+                {
+                    CheckFileExtension(fileName);
+                }
+
+            }
         }
     }
 }
